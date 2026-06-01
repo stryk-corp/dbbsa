907ffac544369ba0e868fcc66e78dad072240388
t0bby-ike <98056031+t0bby-ike@users.noreply.github.com>
2026-06-01 23:58:59 +0100

    mmm

--- neural_village/settings.py
+++ neural_village/settings.py
@@ -22,7 +22,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = [
     host.strip() for host in config(
         'ALLOWED_HOSTS',
-        default='localhost,127.0.0.1,lvh.me,.lvh.me,dbbsa.com,portal.neuralvillage.com,admin.dbbsa.com,sys.neuralvillage.com'
+        default='localhost,127.0.0.1,lvh.me,.lvh.me,*.onrender.com,dbbsa.com,portal.neuralvillage.com,admin.dbbsa.com,sys.neuralvillage.com'
     ).split(',')
     if host.strip()
 ]
