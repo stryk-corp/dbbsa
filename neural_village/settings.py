5b3ef3b304c704d994e3d87b3186da8e23883c7a
t0bby-ike <98056031+t0bby-ike@users.noreply.github.com>
2026-06-12 13:30:26 +0100

    kfjkgyr

--- neural_village/settings.py
+++ neural_village/settings.py
@@ -221,6 +221,7 @@ USE_TZ = True
 # ============================================
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Use standard WhiteNoise storage (Render-compatible)
